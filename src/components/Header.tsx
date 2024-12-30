770d0d886c58ade72498e81dd49bf3bba93327bf
Jordan Roberts <jordanroberts333@icloud.com>
2024-12-30 10:22:55 +0000

    chor: setup svgr

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import SettingsIcon from "../assets/svgs/settings.svg?react";
+
+const Header = ({}) => {
+  return (
+    <header>
+      <div>
+        <SettingsIcon />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
